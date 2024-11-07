6355980baba7ff586760f527226fce53f407dd78
Red0Core <red0.0core@gmail.com>
2024-11-07 14:38:02 +0300

    Забыл добавить инициализацию Tk.Toplevel

--- src/ui/admin_ui.py
+++ src/ui/admin_ui.py
@@ -3,29 +3,29 @@ from tkinter import messagebox, ttk
 from ui.manage_patients_ui import PatientAdminUI
 
 class AdminUI(tk.Toplevel):
-    def __init__(self, root):
-        self.root = root
-        self.root.title("Администратор - Управление")
-        self.root.geometry("500x400")
+    def __init__(self):
+        super().__init__()
+        self.title("Администратор - Управление")
+        self.geometry("500x400")
         
         # Заголовок интерфейса
-        self.label_title = ttk.Label(root, text="Панель администратора", font=("Arial", 16))
+        self.label_title = ttk.Label(self, text="Панель администратора", font=("Arial", 16))
         self.label_title.pack(pady=10)
 
         # Кнопки для управления врачами
-        self.btn_manage_doctors = ttk.Button(root, text="Управление врачами", command=self.manage_doctors)
+        self.btn_manage_doctors = ttk.Button(self, text="Управление врачами", command=self.manage_doctors)
         self.btn_manage_doctors.pack(pady=5)
 
         # Кнопки для управления пациентами
-        self.btn_manage_patients = ttk.Button(root, text="Управление пациентами", command=self.manage_patients)
+        self.btn_manage_patients = ttk.Button(self, text="Управление пациентами", command=self.manage_patients)
         self.btn_manage_patients.pack(pady=5)
 
         # Кнопка для просмотра заявок
-        self.btn_view_requests = ttk.Button(root, text="Просмотр заявок пациентов", command=self.medical_records)
+        self.btn_view_requests = ttk.Button(self, text="Просмотр заявок пациентов", command=self.medical_records)
         self.btn_view_requests.pack(pady=5)
 
         # Кнопка выхода
-        self.btn_exit = ttk.Button(root, text="Выйти", command=self.root.destroy)
+        self.btn_exit = ttk.Button(self, text="Выйти", command=self.destroy)
         self.btn_exit.pack(pady=20)
 
     def manage_doctors(self):
@@ -34,9 +34,7 @@ class AdminUI(tk.Toplevel):
 
     def manage_patients(self):
         # Вызов функции управления списком пациентов
-        manage_patients_window = tk.Toplevel(self.root)
-        PatientAdminUI(manage_patients_window)
-
+        PatientAdminUI()
     def medical_records(self):
         # Просмотр медицинских карт
         messagebox.showinfo("Просмотр заявок", "Здесь будет список заявок пациентов (реализовать в дальнейшем)")
